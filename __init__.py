21bf3d10b845389efc83719adaffe0b6801d9ca9
kjorgecaballero <kja.cruzcaballero@gmail.com>
2025-12-17 19:03:39 -0600

    Initialize Blender CTR Toolkit addon

---
+++ __init__.py
@@ -0,0 +1,22 @@
+bl_info = {
+    "name": "Blender CTR Toolkit",
+    "author": "Jorge Caballero (Siruka)",
+    "version": (0, 1, 0),
+    "blender": (3, 3, 0),
+    "location": "View3D > Sidebar > CTR",
+    "description": "Tools for CTR track development.",
+    "category": "3D View",
+}
+
+import bpy
+
+def register():
+    """Register the entire addon"""
+    print(f"Blender CTR Toolkit loaded")
+
+def unregister():
+    """Unregister the entire addon"""
+    print(f"Blender CTR Toolkit unloaded")
+
+if __name__ == "__main__":
+    register()
