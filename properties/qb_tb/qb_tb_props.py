30d243c5e4b03cefa5448a311f7da3b9ea48ba41
kjorgecaballero <kja.cruzcaballero@gmail.com>
2026-01-08 15:29:47 -0600

    add invalid triblock UVs option and reorder defaults

--- properties/qb_tb/qb_tb_props.py
+++ properties/qb_tb/qb_tb_props.py
@@ -1,28 +1,27 @@
 import bpy
 
 def register():
-    # Find option enum
     bpy.types.Scene.find_option = bpy.props.EnumProperty(
         items=[
-            ('TRIBLOCK', 'Triblocks', 'Find all Triblocks'),
             ('QUADBLOCK', 'Quadblocks', 'Find all Quadblocks'),
+            ('TRIBLOCK', 'Triblocks', 'Find all Triblocks'),
         ],
-        default='TRIBLOCK'
+        default='QUADBLOCK'
     )
     
-    # Select option enum
     bpy.types.Scene.select_option = bpy.props.EnumProperty(
         items=[
+            ('QUADBLOCKS', 'Quadblocks', 'Select all Quadblocks'),
+            ('TRIBLOCKS', 'Triblocks', 'Select all Triblocks'),
             ('ALL_INVALID', 'All Invalid', 'Select all invalid objects'),
             ('INVALID_GEOMETRY', 'Invalid Geometry', 'Select objects with invalid geometry'),
             ('INVALID_UVS', 'Invalid UVs', 'Select objects with invalid UVs'),
             ('DEGENERATED_UVS', 'Degenerated UVs', 'Select objects with degenerated UVs'),
-            ('TRIBLOCKS', 'Triblocks', 'Select all Triblocks'),
-            ('QUADBLOCKS', 'Quadblocks', 'Select all Quadblocks'),
+            ('INVALID_TRIBLOCK_UVS', 'Invalid Triblock UVs', 'Select triblocks with invalid UV arrangement'),
             ('NON_MESH', 'Non-Mesh', 'Select non-mesh objects'),
             ('NGONS', 'NGons', 'Select objects with NGons'),
         ],
-        default='ALL_INVALID'
+        default='QUADBLOCKS'
     )
 
 def unregister():
