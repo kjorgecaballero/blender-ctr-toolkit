ba7938e13df536a6a05ac7a0ccd8c9c6fb9570b6
kjorgecaballero <kja.cruzcaballero@gmail.com>
2026-03-02 22:05:47 -0600

    initialize utilities module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,81 @@
-# EMPTY - just marks as a package
-# If needed expose utils functions in the future do it here
+"""
+Utilities module initialization
+Exporting functions from all utility modules
+"""
+
+from .compat import (
+    get_blender_version,
+    should_use_wm_obj_export,
+    get_export_parameters,
+    apply_scale_to_objects,
+    restore_scale_to_objects,
+    execute_obj_export,
+    get_export_operator_name,
+    has_vertex_colors_support,
+    ensure_objects_in_view_layer,
+    cleanup_temporarily_linked_objects,
+)
+
+from .qb_tb_navigator import (
+    QbTbBlockResult,
+    is_quadblock_center,
+    validate_quadblock_topology,
+    is_triangle_face,
+    is_quad_face,
+    central_face_has_quad_edge,
+    get_quadblock_vertices,
+    get_quadblock_faces,
+    find_adjacent_triangular_faces,
+    is_valid_triblock,
+    get_triblock_faces,
+    get_triblock_vertices,
+    find_qb_tb_with_groups,
+    assign_groups_with_vertex_separation_quads,
+    assign_groups_with_vertex_separation_tris,
+    detect_quadblock_from_faces,
+    detect_triblock_from_faces,
+    detect_block_from_selection,
+    get_faces_by_material_name,
+    is_valid_navigation_point,
+    get_all_navigation_points,
+)
+
+
+__all__ = [
+    # Compatibility functions
+    'get_blender_version',
+    'should_use_wm_obj_export',
+    'get_export_parameters',
+    'apply_scale_to_objects',
+    'restore_scale_to_objects',
+    'execute_obj_export',
+    'get_export_operator_name',
+    'has_vertex_colors_support',
+    'ensure_objects_in_view_layer',
+    'cleanup_temporarily_linked_objects',
+    
+    # Navigation utilities
+    'QbTbBlockResult',
+    'is_quadblock_center',
+    'validate_quadblock_topology',
+    'is_triangle_face',
+    'is_quad_face',
+    'central_face_has_quad_edge',
+    'get_quadblock_vertices',
+    'get_quadblock_faces',
+    'find_adjacent_triangular_faces',
+    'is_valid_triblock',
+    'get_triblock_faces',
+    'get_triblock_vertices',
+    'find_qb_tb_with_groups',
+    'assign_groups_with_vertex_separation_quads',
+    'assign_groups_with_vertex_separation_tris',
+    'detect_quadblock_from_faces',
+    'detect_triblock_from_faces',
+    'detect_block_from_selection',
+    
+    # Constant material utilities
+    'get_faces_by_material_name',
+    'is_valid_navigation_point',
+    'get_all_navigation_points',
+]
