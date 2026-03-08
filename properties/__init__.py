b317ca13451d20b24c99dd22d1419f0a0f7ed78e
kjorgecaballero <kja.cruzcaballero@gmail.com>
2026-03-07 23:46:39 -0600

    update module registration

--- properties/__init__.py
+++ properties/__init__.py
@@ -1,10 +1,16 @@
 from .qb_tb_validator.qb_tb_props import register as register_qb_tb_props, unregister as unregister_qb_tb_props
 from .qb_tb_export.qb_tb_export_props import register as register_qb_tb_export_props, unregister as unregister_qb_tb_export_props
+from .qb_tb_navigator import register as register_qb_tb_navigator_props, unregister as unregister_qb_tb_navigator_props
+from .qb_tb_list import register as register_qb_tb_list_props, unregister as unregister_qb_tb_list_props
 
 def register():
     register_qb_tb_props()
     register_qb_tb_export_props()
+    register_qb_tb_navigator_props()
+    register_qb_tb_list_props()
 
 def unregister():
+    unregister_qb_tb_list_props()
+    unregister_qb_tb_navigator_props()
     unregister_qb_tb_export_props()
     unregister_qb_tb_props()
