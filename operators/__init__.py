8cf8d1d91766f08dae6798f63a1cc2f25d77e83e
kjorgecaballero <kja.cruzcaballero@gmail.com>
2026-03-14 22:39:33 -0600

    Update operator registration

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,15 +1,19 @@
 from .qb_tb_validator.qb_tb_ops import register as register_qb_tb, unregister as unregister_qb_tb
 from .qb_tb_export import register as register_qb_tb_export, unregister as unregister_qb_tb_export
 from .range_box import register_range_box, unregister_range_box
-
+from .qb_tb_navigator import register as register_qb_tb_navigator, unregister as unregister_qb_tb_navigator
+from .qb_tb_list import register as register_qb_tb_list, unregister as unregister_qb_tb_list
 
 def register():
     register_qb_tb()
     register_qb_tb_export()
     register_range_box()
-
+    register_qb_tb_navigator()
+    register_qb_tb_list()
 
 def unregister():
+    unregister_qb_tb_list()
+    unregister_qb_tb_navigator()
     unregister_range_box()
     unregister_qb_tb_export()
     unregister_qb_tb()
