03bd4678165622440231ed53b003b7dc9796f001
kjorgecaballero <kja.cruzcaballero@gmail.com>
2026-03-16 00:01:25 -0600

    Integrate qb_tb_list into UI registration

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,7 +1,13 @@
 from .qb_tb_validator.qb_tb_panel import register as register_qb_tb_ui, unregister as unregister_qb_tb_ui
+from .qb_tb_navigator import register as register_qb_tb_navigator_ui, unregister as unregister_qb_tb_navigator_ui
+from .qb_tb_list import register as register_qb_tb_list_ui, unregister as unregister_qb_tb_list_ui
 
 def register():
     register_qb_tb_ui()
+    register_qb_tb_navigator_ui()
+    register_qb_tb_list_ui()
 
 def unregister():
+    unregister_qb_tb_list_ui()
+    unregister_qb_tb_navigator_ui()
     unregister_qb_tb_ui()
