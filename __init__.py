8cbac9a8f91e61ee26a8540b974973f02c3b8eed
kjorgecaballero <kja.cruzcaballero@gmail.com>
2026-05-07 22:14:26 -0600

    Implement addon_updater module with UI integration and GitHub release detection

--- __init__.py
+++ __init__.py
@@ -10,7 +10,63 @@ bl_info = {
 
 import bpy
 
+
+# Updater integration
+
+from . import addon_updater_ops
+
+
+class CTRToolkitPreferences(bpy.types.AddonPreferences):
+    bl_idname = __package__
+
+    auto_check_update: bpy.props.BoolProperty(
+        name="Auto-check for Update",
+        description="If enabled, auto-check for updates using an interval",
+        default=False,
+    )
+    updater_interval_months: bpy.props.IntProperty(
+        name='Months',
+        description="Number of months between checking for updates",
+        default=0,
+        min=0
+    )
+    updater_interval_days: bpy.props.IntProperty(
+        name='Days',
+        description="Number of days between checking for updates",
+        default=7,
+        min=0,
+    )
+    updater_interval_hours: bpy.props.IntProperty(
+        name='Hours',
+        description="Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23
+    )
+    updater_interval_minutes: bpy.props.IntProperty(
+        name='Minutes',
+        description="Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        addon_updater_ops.update_settings_ui(self, context, layout)
+
+
+
+# Original register / unregister functions
+
 def register():
+    # 1. Register preferences class
+    bpy.utils.register_class(CTRToolkitPreferences)
+
+    # 2. Register updater
+    addon_updater_ops.register(bl_info)
+
+    # 3. Register all built-in modules
     from . import properties
     from . import operators
     from . import ui
@@ -19,7 +75,7 @@ def register():
     operators.register()
     ui.register()
 
-    # Add keymaps
+    # 4. Add keymaps
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name='3D View', space_type='VIEW_3D')
     kmi = km.keymap_items.new("qb_tb.quick_export", 'E', 'PRESS', ctrl=True, shift=True)
@@ -36,7 +92,6 @@ def register():
     )
     kmi2.active = True
 
-    # Ctrl+Shift+D for duplicate with constant reassignment
     kmi3 = km2.keymap_items.new(
         "list.duplicate_selection",
         type='D',
@@ -49,9 +104,9 @@ def register():
 
     print(f"Blender CTR Toolkit v{bl_info['version'][0]}.{bl_info['version'][1]} loaded with Block Navigator and Group Management")
 
-def unregister():
-    from . import ui, operators, properties
 
+def unregister():
+    # 1. Remove keymaps first
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps['3D View']
     for kmi in km.keymap_items:
@@ -64,17 +119,24 @@ def unregister():
         if kmi.idname == "navigator.cursor_select_block":
             km2.keymap_items.remove(kmi)
             break
-
-    for kmi in km2.keymap_items:
         if kmi.idname == "list.duplicate_selection":
             km2.keymap_items.remove(kmi)
             break
 
+    # 2. Unregister built-in modules
+    from . import ui, operators, properties
     ui.unregister()
     operators.unregister()
     properties.unregister()
 
+    # 3. Unregister updater
+    addon_updater_ops.unregister()
+
+    # 4. Unregister preferences class
+    bpy.utils.unregister_class(CTRToolkitPreferences)
+
     print(f"Blender CTR Toolkit v{bl_info['version'][0]}.{bl_info['version'][1]} unloaded")
+
 
 if __name__ == "__main__":
     register()
--- ui/help_utils.py
+++ ui/help_utils.py
@@ -1,28 +1,41 @@
 import bpy
-
+from ..addon_updater_ops import updater
 
 class CTR_HelpUtils:
     DOCS_URL = "https://github.com/kjorgecaballero.github.io/blender-ctr-toolkit"
     TUTORIAL_URL = "https://youtube.com/playlist?list=PLvqykQpD5C2LQq1c7pYH5vWBxTlQ0tVXf"
-    
+    ISSUES_URL = "https://github.com/kjorgecaballero/blender-ctr-toolkit/issues/new/choose"
+
     @classmethod
     def draw_help_buttons(cls, layout):
         row = layout.row(align=True)
         row.alignment = 'RIGHT'
-        
-        doc_op = row.operator(
-            "wm.url_open", 
-            text="", 
-            icon='HELP'
-        )
+
+        # Documentation button
+        doc_op = row.operator("wm.url_open", text="", icon='HELP')
         doc_op.url = cls.DOCS_URL
-        
-        tutorial_op = row.operator(
-            "wm.url_open", 
-            text="", 
-            icon='FILE_MOVIE'
-        )
+
+        # Tutorial button
+        tutorial_op = row.operator("wm.url_open", text="", icon='FILE_MOVIE')
         tutorial_op.url = cls.TUTORIAL_URL
+
+        # Update notification button (turns red when update is ready)
+        update_ready = False
+        if updater and not getattr(updater, 'invalid_updater', True):
+            update_ready = updater.update_ready or False
+
+        update_op = row.operator(
+            "blender_ctr_toolkit.updater_install_popup",
+            text="",
+            icon='ERROR' if update_ready else 'INFO',
+            emboss=True
+        )
+        if update_ready:
+            update_op.alert = True
+
+        # Report Issue button (GitHub)
+        issue_op = row.operator("wm.url_open", text="", icon='URL')
+        issue_op.url = cls.ISSUES_URL
 
 
 def draw_help_buttons(layout):
